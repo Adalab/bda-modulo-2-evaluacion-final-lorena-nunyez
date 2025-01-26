17b5e370c9355fc46a32784e9bf22d77f94a48cd
lorena.nunyez <lorena.nunyez@gmail.com>
2025-01-26 10:35:12 +0100

    v3_ejercicio_tecnico

--- ejercicio_tecnico.sql
+++ ejercicio_tecnico.sql
@@ -1,7 +1,5 @@
 /* EJERCICIOS
 
-
-
 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
 17. Encuentra el título de todas las películas que son de la misma categoría que "Family".
 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.
@@ -38,91 +36,161 @@ clasificación es rating y categoría es el name de la tabla category.
 USE sakila;
 
 -- 01. Selecciona todos los nombres de las películas sin que aparezcan duplicados.
-SELECT DISTINCT title
-FROM film;
+SELECT DISTINCT
+    title
+FROM
+    film;
 
 -- 02. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
-SELECT title, rating
-FROM film
-WHERE rating = 'PG-13'; -- donde X sea Y
+SELECT 
+    title, rating
+FROM
+    film
+WHERE
+    rating = 'PG-13'; -- donde X sea Y
 
 
 -- 03. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
-SELECT title, description
-FROM film
-WHERE description LIKE '%amazing%'; -- donde X contenga Y
+SELECT 
+    title, description
+FROM
+    film
+WHERE
+    description LIKE '%amazing%'; -- donde X contenga Y
 
 -- 04. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
-SELECT title, length
-FROM film
+SELECT 
+    title, length
+FROM
+    film
 WHERE length>120; -- 120 no incluidos
 
 -- 05. Encuentra los nombres de todos los actores, muestralos en una sola columna que se llame
 -- ----nombre_actor y contenga nombre y apellido.
-SELECT CONCAT(first_name,' ', last_name) 
-AS nombre_actor -- las '' indica todos los registros
-FROM actor; 
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS nombre_actor
+FROM
+    actor; 
 
 -- 06. Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido.
-SELECT first_name, last_name
-FROM actor
-WHERE last_name LIKE '%Gibson%'; -- donde X contenga Y
+SELECT 
+    first_name, last_name
+FROM
+    actor
+WHERE
+    last_name LIKE '%Gibson%'; -- donde X contenga Y
 
 -- 07. Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20.
-SELECT first_name, actor_id
-FROM actor
-WHERE actor_id BETWEEN 10 AND 20
+SELECT 
+    first_name, actor_id
+FROM
+    actor
+WHERE
+    actor_id BETWEEN 10 AND 20;
 
 -- 08. Encuentra el título de las películas en la tabla film que no tengan clasificacion "R" ni "PG-13".
-SELECT title, rating
-FROM film
-WHERE rating NOT IN('PG-13','R');
+SELECT 
+    title, rating
+FROM
+    film
+WHERE
+    rating NOT IN ('PG-13' , 'R');
 
 -- 09. Encuentra la cantidad total de películas en cada clasificación(rating) de la tabla film y muestra la
 -- --- clasificación junto con el recuento.
-SELECT rating,
-COUNT(rating) AS total_peliculas
-FROM film
+SELECT 
+    rating, COUNT(rating) AS total_peliculas
+FROM
+    film
 GROUP BY rating; -- primero marcamos pq queremos agrupar (rating) luego le pedimos que cuente cuantas hay de cada tipo.
 
 -- 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su
 -- --- nombre y apellido junto con la cantidad de películas alquiladas.
 SELECT 
-    c.customer_id, 
+    c.customer_id,
     c.first_name,
     c.last_name,
-    COUNT(r.rental_id) AS total_peliculas 
-FROM customer c
-JOIN rental r ON c.customer_id = r.rental_id
-GROUP BY c.customer_id, c.first_name, c.last_name;
+    COUNT(r.rental_id) AS total_peliculas
+FROM
+    customer c
+	JOIN
+    rental r ON c.customer_id = r.rental_id
+GROUP BY c.customer_id , c.first_name , c.last_name;
 
 -- 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la
 -- --- categoría junto con el recuento de alquileres.
 
 SELECT 
-	r.rental_id
-    r.inventory_id
-    f.rating
-GROUP BY rating
-    
+    c.name AS categorias,
+    COUNT(r.rental_id) AS numero_alquileres
+FROM 
+	category c
+	INNER JOIN film_category fc 
+    ON c.category_id = fc.category_id
+	INNER JOIN inventory i 
+    ON i.film_id = fc.film_id
+	INNER JOIN rental r 
+    ON r.inventory_id = i.inventory_id
+GROUP BY categorias;
+
 -- 12. Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y
 -- --- muestra la clasificación junto con el promedio de duración.
 SELECT 
-    rating AS clasificacion,
-    AVG (length) AS promedio_duracion
-FROM film
+    rating AS clasificacion, 
+    AVG(length) AS promedio_duracion
+FROM
+    film
 GROUP BY rating
 ORDER BY promedio_duracion;
 
 -- 13. Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
+SELECT 
+    a.first_name AS nombre_actor,
+    a.last_name AS apellido_actor
+FROM 
+    actor a
+JOIN 
+    film_actor fa ON a.actor_id = fa.actor_id
+JOIN 
+    film f ON fa.film_id = f.film_id
+WHERE 
+    f.title = 'Indian Love';
 
 -- 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
 
+SELECT 
+title AS titulo_pelicula
+FROM film
+WHERE
+	description LIKE '%dog%'
+	OR description LIKE '%cat%';
+
+-- para comprobar:
+SELECT 
+title AS titulo_pelicula,
+description AS descripcion_dog_cat
+FROM film
+WHERE
+	description LIKE '%dog%'
+	OR description LIKE '%cat%';
+
 -- 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.
+
+SELECT 
+    a.actor_id AS id_actor,
+    a.first_name AS nombre,
+    a.last_name AS apellido
+FROM 
+    actor a
+LEFT JOIN 
+    film_actor fa ON a.actor_id = fa.actor_id
+WHERE 
+    fa.film_id IS NULL;
+
 
 SELECT * FROM inventory;
 SELECT * FROM rental;
-SELECT * FROM category;
+SELECT * FROM actor;
 SELECT * FROM film;
 
 
